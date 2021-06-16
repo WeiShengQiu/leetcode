66d7187fcba0c3cb07c68f6c8f4fd42555e8a371
汪诗祺 <wangshiqi@cestc.cn>
2021-06-16 21:08:55 +0800

    6月16日练习题，动态规划

---
+++ src/offer/FindGreatestSumOfSubArray.java
@@ -0,0 +1,27 @@
+package offer;
+
+public class FindGreatestSumOfSubArray {
+    public static int FindGreatestSumOfSubArray(int[] array) {
+        int[] dp = new int[array.length];
+        dp[0] = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            int temp = dp[i - 1] + array[i];
+            if (temp > array[i]) {
+                dp[i] = temp;
+            } else {
+                dp[i] = array[i];
+            }
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, -2, 3, -1, 8, -3, 2};
+        System.out.println(FindGreatestSumOfSubArray(array));
+    }
+}
