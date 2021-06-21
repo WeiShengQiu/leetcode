b798df3a674b8bcd63c8ce8e34f3130576be3ee8
wangshiqi <358235327@qq.com>
2021-06-21 22:59:16 +0800

    6月21日练习题 替换空字符串

---
+++ src/offer/hard/ReplaceSpace.java
@@ -0,0 +1,7 @@
+package offer.hard;
+
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ", "%20");
+    }
+}
---
+++ src/offer/normal/ReConstructBinaryTree.java
@@ -0,0 +1,17 @@
+package offer.normal;
+
+public class ReConstructBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+
+    }
+}
