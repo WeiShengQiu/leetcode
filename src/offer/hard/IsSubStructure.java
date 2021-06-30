59b9be93c655007e34c482cf667c852292412740
wangshiqi <358235327@qq.com>
2021-06-30 21:45:02 +0800

    6月30日练习，切换到LeetCode练习

---
+++ src/offer/hard/IsSubStructure.java
@@ -0,0 +1,23 @@
+package offer.hard;
+
+public class IsSubStructure {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}
